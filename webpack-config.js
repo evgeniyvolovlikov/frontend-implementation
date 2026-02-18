5520ca4ce8da322df8176e24bdde385e4ddcb533
Евгений Воловликов <evgenyvolovlikov@gmail.com>
2026-02-18 16:00:57 +0300

    initialize base webpack setup

---
+++ webpack-config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'index.js'),
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: '[name].[contenthash].js',
+		clean: true,
+	},
+};
